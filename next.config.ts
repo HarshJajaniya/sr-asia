e3e11a57b952b20a760f3f917c3aee92eb2621fd
parthgupta9 <parth615gupta@gmail.com>
2025-06-16 13:37:54 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
+        protocol: 'https',
+        hostname: 'srasia-backend.onrender.com',
+      
         pathname: '/uploads/**',
       },
     ],
