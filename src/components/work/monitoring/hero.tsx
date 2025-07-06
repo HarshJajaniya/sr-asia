b5a4bb2d20598699a013aa49aeeec8576ed7117c
HarshJajaniya <ajjujajaniya@gmail.com>
2025-07-06 15:53:28 +0530

    commit

--- src/components/work/monitoring/hero.tsx
+++ src/components/work/monitoring/hero.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <section className="relative h-[600px] md:h-[600px] overflow-hidden">
+    <section className="relative h-[600px] mb-[75px] md:h-[600px] overflow-hidden">
       {/* Video Background */}
       <div className="absolute inset-0 w-full h-full">
         <video
