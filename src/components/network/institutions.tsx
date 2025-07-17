dd7ff6706a092ed0bb4388133214dc1f24670159
HarshJajaniya <ajjujajaniya@gmail.com>
2025-07-18 00:34:58 +0530

    commit

--- src/components/network/institutions.tsx
+++ src/components/network/institutions.tsx
@@ -80,17 +80,22 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex mb-[75px] items-center justify-center bg-[#EEFAFE] p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {universities.map((uni, index) => (
-          <UniversityCard
-            key={index}
-            imageSrc={uni.imageSrc}
-            imageAlt={uni.imageAlt}
-            title={uni.title}
-            description={uni.description}
-          />
-        ))}
+    <div>
+      <h1 className="text-3xl md:text-[40px] font-bold text-center mb-12 text-gray-800">
+        INSTITUTIONAL COLLABORATIONS
+      </h1>
+      <div className="flex mb-[75px] items-center justify-center bg-[#EEFAFE] p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {universities.map((uni, index) => (
+            <UniversityCard
+              key={index}
+              imageSrc={uni.imageSrc}
+              imageAlt={uni.imageAlt}
+              title={uni.title}
+              description={uni.description}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
