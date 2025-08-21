c4d6b9485eb0a23dc407bc35c2b7fd1b0e341680
parthgupta9 <parth615gupta@gmail.com>
2025-08-21 22:05:29 +0530

    google analtics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 
 import "./globals.css";
 import { Roboto_Flex  } from 'next/font/google';
@@ -24,6 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+ <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M53CB0JDJ2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M53CB0JDJ2');
+          `}
+        </Script>
+      </head>
       <body
         className={`${robotoFlex.className} ${robotoFlex.className} antialiased bg-[#EEFAFE]`}
       >
